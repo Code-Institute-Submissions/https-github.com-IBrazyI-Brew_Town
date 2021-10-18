9c570d781ce3ccf0725d2762806084c3b5a3cf54
IBrazyI <braz.josh@yahoo.co.uk>
2021-10-18 11:54:39 +0000

    Registred models within the admin file in the products app£

--- products/admin.py
+++ products/admin.py
@@ -1,3 +1,32 @@
 from django.contrib import admin
+from .models import Category, Product
 
-# Register your models here.
+#Used to display the data of the products in an easy to read format within the database.
+class ProductAdmin(admin.ModelAdmin):
+    list_display = (
+        'sku',
+        'name',
+        'category',
+        'price',
+        'rating',
+        'image',
+
+    )
+
+    ordering = ('sku',)
+
+
+#Used to display both names for the categories within the database.
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = (
+        'name',
+        'friendly_name',
+        
+    )
+
+
+
+#Allows the admin to display the data and formating of the database.
+admin.site.register(Product, ProductAdmin)
+admin.site.register(Category, CategoryAdmin)
+
