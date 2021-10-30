33faf16e9fb0dc0172146666f4a39968aee68e7f
IBrazyI <braz.josh@yahoo.co.uk>
2021-10-30 09:13:11 +0000

    Sucessfully created view to display the shopping bag page and functions

--- shopping_bag/views.py
+++ shopping_bag/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
 
-
+def view_bag(request):
+    """Renders the shopping bag page, will also handle all shopping bag processes"""
+    
+    return render(request, 'shopping_bag/shopping_bag.html')
