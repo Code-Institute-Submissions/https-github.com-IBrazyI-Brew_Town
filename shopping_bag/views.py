83d4f731cc3f22fca944665c34c8b946107af83c
IBrazyI <braz.josh@yahoo.co.uk>
2021-11-02 07:31:28 +0000

    Allowed user product selections to be stored within the session, works as intended, products selected and quanity displaying in console

--- shopping_bag/views.py
+++ shopping_bag/views.py
@@ -1,7 +1,22 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
 def view_bag(request):
     """Renders the shopping bag page, will also handle all shopping bag processes"""
     
     return render(request, 'shopping_bag/shopping_bag.html')
 
+def add_to_bag(request, item_id):
+    """ Add items to the shopping bag, able to add different quantitys """
+
+    quantity = request.POST.get('quanity')
+    redirect_url = request.POST.get('redirect_url')
+    bag = request.session.get('bag', {})
+
+    if item_id in list(bag.keys()):
+        bag[item_id] += quantity
+    else:
+        bag[item_id] = quantity
+
+    request.session['bag'] = bag
+    print(request.session['bag'])
+    return redirect(redirect_url)
