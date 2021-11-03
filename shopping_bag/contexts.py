47e602d7083717168bb01482b0adf1f5e770b8d8
IBrazyI <braz.josh@yahoo.co.uk>
2021-11-03 20:25:17 +0000

    Shopping bag working as intended, saving items and displaying grand total at top of page

--- shopping_bag/contexts.py
+++ shopping_bag/contexts.py
@@ -8,35 +8,38 @@ def shopping_bag_contents(request):
     bag_items = []
     total = 0
     product_count = 0
-    bag = request.session.get('bag',{})
+    bag = request.session.get('bag', {})
+    
 
     for item_id, quantity in bag.items():
+        #confused to why this makes it work...
+        quantity = int(quantity)
         product = get_object_or_404(Product, pk=item_id)
         total += quantity * product.price
         product_count += quantity
         bag_items.append({
             'item_id': item_id,
-            'quantity': quaintity,
+            'quantity': quantity,
             'product': product,
         })
 
     if total < settings.FREE_DELIVERY_THRESHOLD:
-        delivery = total * Decimal(settings.STANDARD_DELIVERY_PERCENTAGE/100)
+        delivery = total * Decimal(settings.STANDARD_DELIVERY_PERCENTAGE / 100)
         free_delivery_delta = settings.FREE_DELIVERY_THRESHOLD - total
     else:
         delivery = 0
         free_delivery_delta = 0
-
+    
     grand_total = delivery + total
-
+    
     context = {
-        'bag_items' : bag_items,
-        'total' : total,
-        'product_count' : product_count,
-        'delivery' : delivery,
-        'free_delivery_delta' : free_delivery_delta,
-        'free_delivery_threshold' : settings.FREE_DELIVERY_THRESHOLD,
-        'grand_total' : grand_total,
+        'bag_items': bag_items,
+        'total': total,
+        'product_count': product_count,
+        'delivery': delivery,
+        'free_delivery_delta': free_delivery_delta,
+        'free_delivery_threshold': settings.FREE_DELIVERY_THRESHOLD,
+        'grand_total': grand_total,
     }
 
     return context
--- shopping_bag/views.py
+++ shopping_bag/views.py
@@ -8,7 +8,7 @@ def view_bag(request):
 def add_to_bag(request, item_id):
     """ Add items to the shopping bag, able to add different quantitys """
 
-    quantity = request.POST.get('quanity')
+    quantity = int(request.POST.get('quantity'))
     redirect_url = request.POST.get('redirect_url')
     bag = request.session.get('bag', {})
 
