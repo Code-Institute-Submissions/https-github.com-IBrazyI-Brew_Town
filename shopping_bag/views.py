d3e80aa630c147558f973b0e9084c76d858d761f
IBrazyI <braz.josh@yahoo.co.uk>
2021-11-05 16:56:47 +0000

    Created views to update and delete items within the shopping bag

--- shopping_bag/views.py
+++ shopping_bag/views.py
@@ -1,9 +1,10 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, reverse, HttpResponse
 
 def view_bag(request):
     """Renders the shopping bag page, will also handle all shopping bag processes"""
     
     return render(request, 'shopping_bag/shopping_bag.html')
+
 
 def add_to_bag(request, item_id):
     """ Add items to the shopping bag, able to add different quantitys """
@@ -19,3 +20,30 @@ def add_to_bag(request, item_id):
 
     request.session['bag'] = bag
     return redirect(redirect_url)
+
+
+def edit_bag(request, item_id):
+    """ Adjust the ammount of each item within the shopping bag """
+
+    quantity = int(request.POST.get('quantity'))
+    bag = request.session.get('bag', {})
+
+    if quantity > 0:
+        bag[item_id] = quantity
+    else:
+        bag.pop(item_id)
+    
+    request.session['bag'] = bag
+    return redirect(reverse('view_bag'))
+
+def remove_from_bag(request, item_id):
+    """ Remove an item from the shopping bag """
+
+    try:
+        bag = request.session.get('bag', {})
+        bag.pop(item_id)
+
+        request.session['bag'] = bag
+        return HttpResponse(status=200)
+    except Exception as e:
+        return HttpResponse(status=500)
