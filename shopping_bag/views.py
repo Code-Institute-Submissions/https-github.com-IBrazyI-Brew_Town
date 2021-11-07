6ef06c4b0cb246515c4838aa3371139374951f6b
IBrazyI <braz.josh@yahoo.co.uk>
2021-11-07 11:41:35 +0000

    Added styling to page title, with icon. Also made css rules for links to inherit styling rather than showing hyperlinks

--- shopping_bag/views.py
+++ shopping_bag/views.py
@@ -1,4 +1,6 @@
 from django.shortcuts import render, redirect, reverse, HttpResponse
+from django.contrib import messages
+from products.models import Product
 
 def view_bag(request):
     """Renders the shopping bag page, will also handle all shopping bag processes"""
@@ -9,6 +11,7 @@ def view_bag(request):
 def add_to_bag(request, item_id):
     """ Add items to the shopping bag, able to add different quantitys """
 
+    product = Product.objects.get(pk=item_id)
     quantity = int(request.POST.get('quantity'))
     redirect_url = request.POST.get('redirect_url')
     bag = request.session.get('bag', {})
@@ -17,6 +20,7 @@ def add_to_bag(request, item_id):
         bag[item_id] += quantity
     else:
         bag[item_id] = quantity
+        messages.success(request, f'Added {product.name} to bag')
 
     request.session['bag'] = bag
     return redirect(redirect_url)
