8182bce20041738a3023e90c0563141055e7eff5
IBrazyI <braz.josh@yahoo.co.uk>
2021-11-09 11:39:40 +0000

    Adeed methods to order and orderlineitem models

--- checkout/models.py
+++ checkout/models.py
@@ -24,10 +24,42 @@ class Order(models.Model):
     order_total = models.DecimalField(max_digits=10, decimal_places=2, null=False, default=0)
     grand_total = models.DecimalField(max_digits=10, decimal_places=2, null=False, default=0)
 
+    def _create_order_number(self):
+        """ Creates a random, unique order number using UUID """
 
-class OrderItem(models.Model):
+        return uuid.uuid4().hex.upper()
+
+    def update_total(self):
+        """ Updates the grand total of the order each time a new item is added """
+        self.order_total = self.lineitems.aggregate(Sum('lineitem_total'))['lineitem_total__sum']
+        if self.order_total < settings.FREE_DELIVERY_THRESHOLD:
+            self.delivery_cost = self.order_total * settings.STANDARD_DELIVERY_PERCENTAGE / 100
+        else:
+            self.delivery_cost = 0
+        self.grand_total = self.order_total + delivery_cost
+        self.save()
+
+
+    def save(self, *args, **kwargs):
+        """ Override orignal save method, allowing product to be given an order number if it does not already have one """
+        if not self.order_number:
+            self.order_number = self._create_order_number()
+        super().save(*args, **kwargs)
+
+    def __str__(self):
+        return self.order_number
+
+class OrderLineItem(models.Model):
     """ Will be made for each item within the shopping bag then attached to the order itself """
-    order = models.ForeignKey(Order, null=false, blank=False, on_delete=models.CASCADE, related_name='items')
+    order = models.ForeignKey(Order, null=false, blank=False, on_delete=models.CASCADE, related_name='lineitems')
     product = models.ForeignKey(Product, null=false, blank=False, on_delete=models.CASCADE)
     quantity = models.IntegerField(null=False, blank=False, default=0)
     item_total = models.DecimalField(max_digits=6, decimal_places=2, null=False, blank=False, editable=False)
+
+    def save(self, *args, **kwargs):
+        """ Override orignal save method, allowing product to be given an order number if it does not already have one """
+        self.lineitem_total = self.product.price * self.quantity
+        super().save(*args, **kwargs)
+
+        def __str__(self):
+            return f'SKU {self.product.sku} on order {self.order.order_number}'
