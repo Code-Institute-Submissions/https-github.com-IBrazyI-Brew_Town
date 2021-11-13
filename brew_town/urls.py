169762eb561e57d7d1b03540652430b4f606c736
IBrazyI <braz.josh@yahoo.co.uk>
2021-11-13 15:08:30 +0000

    Completed stripe implmentation

--- brew_town/urls.py
+++ brew_town/urls.py
@@ -26,5 +26,4 @@ urlpatterns = [
     path('products/', include('products.urls')),
     path('shopping_bag/', include('shopping_bag.urls')),
     path('checkout/', include('checkout.urls')),
-
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
