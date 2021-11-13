3c881cd3feeafa4d903b9cfc7553bb14256fb26e
IBrazyI <braz.josh@yahoo.co.uk>
2021-11-13 10:05:25 +0000

    Added validation errors for stripe payments

--- checkout/static/checkout/js/stripe_elements.js
+++ checkout/static/checkout/js/stripe_elements.js
@@ -1,6 +1,10 @@
+// Link to stripe account//
+
 var stripe_public_key = $('#id_stripe_public_key').text().slice(1, -1);
 var client_secret = $('#id_client_secret').text().slice(1, -1);
 var stripe = Stripe(stripe_public_key);
+
+// Stripe elements //
 var elements = stripe.elements();
 var style = {
     base: {
@@ -21,3 +25,21 @@ var style = {
 var card = elements.create('card', {style: style});
 
 card.mount('#card-element');
+
+// STRIPE FUNCTIONALITY //
+
+// Validation Errors //
+card.addEventListener('change', function (event) {
+    var errorDiv = document.getElementById('card-errors');
+    if (event.error) {
+        var html = `
+            <span class="icon" role="alert">
+                <i class="fas fa-times"></i>
+            </span>
+            <span>${event.error.message}</span>
+        `;
+        $(errorDiv).html(html);
+    } else {
+        errorDiv.textContent = '';
+    }
+});
