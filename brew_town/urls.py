4888bcb53341e19c5344743ab208d0460f875d0d
IBrazyI <braz.josh@yahoo.co.uk>
2021-11-17 16:48:13 +0000

    Added reviews urls to project level url file

--- brew_town/urls.py
+++ brew_town/urls.py
@@ -27,4 +27,5 @@ urlpatterns = [
     path('shopping_bag/', include('shopping_bag.urls')),
     path('checkout/', include('checkout.urls')),
     path('profile/', include('profiles.urls')),
+    path('reviews/', include('reviews.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
