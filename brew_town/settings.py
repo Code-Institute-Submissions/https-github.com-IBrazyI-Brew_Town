72aec35bdf0670eb371a086292b7da83aa09fc30
IBrazyI <braz.josh@yahoo.co.uk>
2021-11-18 14:57:13 +0000

    Changed static root

--- brew_town/settings.py
+++ brew_town/settings.py
@@ -187,7 +187,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
-STATIC_ROOT = BASE_DIR / 'staticfiles'
+STATIC_ROOT = BASE_DIR / 'static'
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 MEDIA_URL = '/media/'
