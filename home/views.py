703d011179461fbc2ddf2dc59ba7aeca3d85752c
IBrazyI <braz.josh@yahoo.co.uk>
2021-11-19 17:29:58 +0000

    Added staticfiles

--- home/views.py
+++ home/views.py
@@ -1,6 +1,17 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.core.mail import send_mail
+from django.conf import settings
 
 # Create your views here.
 def index(request):
     return render(request, 'home/index.html')
+
+
+def newsletter_email(request, email):
+    if method == GET:
+        email = request.GET.get('email')
+
+        send_mail('Newsletter',
+                    'Welcome {{ user_email }} to the brewtown newsletter!',
+                     settings.DEFAULT_FROM_EMAIL,
+                      user_email)
