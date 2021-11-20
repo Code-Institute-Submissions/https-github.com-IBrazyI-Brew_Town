668a89a9b95c9fe5ca8eb8506f69b3861f9c677f
IBrazyI <braz.josh@yahoo.co.uk>
2021-11-20 11:55:32 +0000

    Added link to reviews page to retun the user home if there are reviews to see

--- brew_town/settings.py
+++ brew_town/settings.py
@@ -190,7 +190,7 @@ STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
 STATIC_ROOT = BASE_DIR / 'static'
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
-MEDIA_URL = '/static/media/'
+MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 #Stripe
