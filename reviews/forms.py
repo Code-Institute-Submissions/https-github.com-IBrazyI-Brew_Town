1b1e6bb9acc8b82df73f62308a3ccc6745983d42
IBrazyI <braz.josh@yahoo.co.uk>
2021-11-20 16:41:35 +0000

    Added reviewform so users can post thier own reviews

--- reviews/forms.py
+++ reviews/forms.py
@@ -1,5 +1,11 @@
-from .models import Comment
+from .models import Comment, Review
 from django import forms
+
+
+class ReviewForm(forms.ModelForm):
+    class Meta:
+        model = Review
+        fields = ('title', 'author', 'created_on', 'content',)
 
 
 class CommentForm(forms.ModelForm):
